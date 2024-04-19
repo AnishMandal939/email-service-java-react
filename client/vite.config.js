1a1cb491a401d8e2086357ebe47d83d8945dba2e
AnishMandal939 <mandalktaanees12@gmail.com>
2024-04-20 01:55:26 +0545

    fix - dotenv configuration for process.env not defined issue solved, and dotenv configurations setups

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // define process env
+  define: {
+    // eslint-disable-next-line no-undef
+    'process.env': process.env
+  }
 })
